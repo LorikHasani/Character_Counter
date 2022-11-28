8b87b4cc2dcbcec48b3013ad630dc3a43a5a6fce
loriki7 <kikpubgm@gmail.com>
2022-11-28 20:50:57 +0200

    Final touches

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textareaEl = document.getElementById("textarea");
 const totalCounterEl = document.getElementById("total-counter");
 const remainingCounterEl = document.getElementById("remaining-counter");
+const reachLimit = document.getElementById("limit");
 
 textareaEl.addEventListener("keyup", () => {
     updateCounter();
@@ -11,6 +12,10 @@ updateCounter()
 function updateCounter() {
     totalCounterEl.innerText = textareaEl.value.length;
     remainingCounterEl.innerText = textareaEl.getAttribute("maxLength") - textareaEl.value.length;
+
+    if(textareaEl.value.length == "50"){ 
+        reachLimit.classList.remove("hiden");
+    }
 }
 
 
